3c1df37fb5ce953948484fbaac96fe347e8a13dd
vortex204 <jespinal1193@gmail.com>
2023-05-01 20:45:29 -0400

    celsius to fahrenheit

---
+++ c_tof.js
@@ -0,0 +1,28 @@
+function convertCtoF(celsius) {
+const fahrenheit = (celsius * 9/5) + 32;
+return fahrenheit;
+}
+
+let currentTemp = 100;
+fahrenheitTemp = convertCtoF(currentTemp)
+console.log(`${currentTemp} °C is ${fahrenheitTemp} °F`); 
+
+currentTemp = 45;
+fahrenheitTemp = convertCtoF(currentTemp)
+console.log(`${currentTemp} °C is ${fahrenheitTemp} °F`); 
+
+currentTemp = 19;
+fahrenheitTemp = convertCtoF(currentTemp)
+console.log(`${currentTemp} °C is ${fahrenheitTemp} °F`); 
+
+currentTemp = 0;
+fahrenheitTemp = convertCtoF(currentTemp)
+console.log(`${currentTemp} °C is ${fahrenheitTemp} °F`); 
+
+currentTemp = -7;
+fahrenheitTemp = convertCtoF(currentTemp)
+console.log(`${currentTemp} °C is ${fahrenheitTemp} °F`); 
+
+currentTemp = -40;
+fahrenheitTemp = convertCtoF(currentTemp)
+console.log(`${currentTemp} °C is ${fahrenheitTemp} °F`); 
